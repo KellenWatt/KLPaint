7c01e421b6d53f326ad6c8f9f90381b24ee21189
Kellen Watt <kellen.watt@knowledgelake.com>
2017-06-06 11:20:56 -0500

    Add more tool functionality

--- assets/js/Paint.js
+++ assets/js/Paint.js
@@ -9,6 +9,9 @@ var Paint = (function(document) {
     var weight;
     var currentTool;
     var fill = false;
+
+    var image;
+
     var _layerid = 0;
 
 
@@ -35,6 +38,14 @@ var Paint = (function(document) {
             mouse.x = e.pageX - this.offsetLeft;
             mouse.y = e.pageY - this.offsetTop;
         });
+    }
+
+    function addAlpha(hex, num) {
+        var red = parseInt(hex.substring(1,3), 16);
+        var green = parseInt(hex.substring(3,5), 16);
+        var blue = parseInt(hex.substring(5,7), 16);
+        var alpha = parseInt(num, 16) / 255;
+        return "rgba("+red+","+green+","+blue+","+alpha+")";
     }
 
     Paint.prototype.setPrimary(primary) {
@@ -100,50 +111,71 @@ var Paint = (function(document) {
 
 
     // Begin drawing functions
-    Paint.prototype.drawPencil() {
-        cxt.lineto(mouse.x, mouse.y);
-        cxt.stroke();
+    function drawPencil() {
+        ctx.lineto(mouse.x, mouse.y);
+        ctx.stroke();
         points.push(new Point(mouse.x, mouse.y));
     }
 
-    Paint.prototype.drawBrush() {
+    function drawBrush() {
         // Fill this in later because I don't know the exact implementation.
+        var dist = Math.sqrt(Math.pow(mouse.x - mouseLock.x, 2)
+                             + Math.pow(mouse.y - mouseLock.y,2));
+        var angle = Math.atan2(mouse.x - mouseLock.x, mouse.y - mouseLock.y);
+        ctx.lineJoin = "round";
+        ctx.lineCap = "round";
+
+        for(var i=0; i < dist; i += Math.ceil(weight/8)) {
+            x = mouseLock.x + (Math.sin(angle) * i);
+            y = mouseLock.y + (Math.cos(angle) * i);
+
+            var radgrad = ctx.createRedialGradient(x, y, weight/4, x, y, weight/2);
+
+            radgrad.addColorStop(0, addAlpha(colors[0], 1));
+            radgrad.addColorStop(0.5, addAlpha(colors[0], 0.5));
+            radgrad.addColorStop(1, addAlpha(colors[0], 0);
+
+            ctx.fillsytle = radgrad;
+            ctx.fillRect(x - weight/2, y - weight/2, weight, weight);
+        }
+
+        mouseLock = mouse;
     }
 
-    Paint.prototype.drawCircle() {
-        cxt.clearRect(0, 0, workspace.width, workspace.height);
-        cxt.beginPath();
+    function drawCircle() {
+        ctx.clearRect(0, 0, workspace.width, workspace.height);
+        ctx.beginPath();
         var radius = Math.sqrt(Math.pow(mouse.x - mouseLock.x, 2) +
                                Math.pow(mouse.y - mouseLock.y, 2));
         context.arc(mouseLock.x, mouseLock.y, radius, 0, 2*Math.PI);
         if(fill) {
-            cxt.fill();
+            ctx.fill();
         }else {
-            cxt.stroke();
+            ctx.stroke();
         }
     }
 
-    Paint.prototype.drawSqure() {
-        cxt.clearRect(0, 0, workspace.width, workspace.height);
-        cxt.beginPath();
-        cxt.rect(mouseLock.x, mouseLock.y,
+    function drawSqure() {
+        ctx.clearRect(0, 0, workspace.width, workspace.height);
+        ctx.beginPath();
+        ctx.rect(mouseLock.x, mouseLock.y,
                  mouse.x - mouseLock.x, mouse.y - mouseLock.y);
         if(fill) {
-            cxt.fill();
+            ctx.fill();
         }else {
-            cxt.stroke();
+            ctx.stroke();
         }
     }
 
-    Paint.prototype.drawLine() {
-        cxt.clearRect(0, 0, workspace.width, workspace.height);
-        cxt.beginPath();
-        cxt.moveTo(mouseLock.x, mouseLock.y);
-        cxt.lineTo(mouse.x, mouse.y);
-        cxt.stroke();
+    function drawLine() {
+        ctx.clearRect(0, 0, workspace.width, workspace.height);
+        ctx.beginPath();
+        ctx.moveTo(mouseLock.x, mouseLock.y);
+        ctx.lineTo(mouse.x, mouse.y);
+        ctx.stroke();
     }
 
-    Paint.protype.drawText() { // need to work on this more
+    function drawText() { // need to work on this more
         // var textIn = document.createElement("input");
         // textIn.setAttribute("type", "text");
         // textIn.style.position = "absolute";
@@ -154,37 +186,52 @@ var Paint = (function(document) {
         // textIn.addEventListener("blur", function() {
         //     var x = mouseLock.x;
         //     var y = mouseLock.y;
-        //     cxt.fillText(this.value, x, y, )
+        //     ctx.fillText(this.value, x, y, )
         // });
         //
         // workspace.parentNode.appendChild(textIn);
     }
 
-    Paint.prototype.erase() {
+    function erase() {
         var context = currentLayer.getCanvasContext();
         context.save();
         context.arc(mouse.x, mouse.y, weight, 0, 2*Math.PI, true);
         context.clip();
-        context.cleearRect(mouse.x - weight, mouse.y - weight, weight*2, weight*2);
+        context.clearRect(mouse.x - weight, mouse.y - weight, weight*2, weight*2);
         context.restore();
     }
 
-
-    Paint.prototype.chooseColor() {
-
+    function findColor() {
+        var pix = currentLayer.getCanvasContext()
+                    .getImageData(mouse.x, mouse.y, 1, 1).data;
+        var red = parseInt(""+pix[0], 16);
+        var green = parseInt(""+pix[1], 16);
+        var blue = parseInt(""+pix[2], 16);
+        colors[0] = "#" + red + green + blue;
     }
 
-    Paint.prototype.drawImage() {
+    function chooseColor() {
+        // probably not needed
+    }
 
+    function drawImage() {
+        ctx.clearRect(0, 0, workspace.width, workspace.height);
+        ctx.drawImage(image, mouseLock.x, mouseLock.y,
+                             mouse.x - mouseLock.x, mouse.y - mouseLock.y);
     }
 
 
     Paint.prototype.init() {
         var tool = this.tool;
+        var colorChooser = document.createElement("input");
+        colorChooser.setAttribute("type", "color");
+        colorChooser.addEventListener("change", function() {
+            colors[0] = this.value;
+        });
 
         this.workspace.addEventListener("mousedown", function() {
-            cxt.beginPath();
-            cxt.moveTo(this.mouse.x, this.mouse.y);
+            ctx.beginPath();
+            ctx.moveTo(this.mouse.x, this.mouse.y);
             mouseLock.x = mouse.x;
             mouseLock.y = mouse.y;
 
@@ -193,6 +240,7 @@ var Paint = (function(document) {
                 canvas.addEventListener("mousemove", drawPencil);
             break;
             case "brush":
+                ctx.save();
                 canvas.addEventListener("mousemove", drawBrush);
             break;
             case "circle":
@@ -211,16 +259,19 @@ var Paint = (function(document) {
                 canvas.addEventListener("mousemove", erase);
             break;
             case "dropper":
-                document.getElementById("primary").click();
+                // canvas.addEventListener("click", findColor);
+                // handled in the mouseup event
             break;
             case "color":
-                // canvas.addEventListener("mousemove", drawBrush);
+                // not likely to be needed
+                // will be handled in the mouseup event
             break;
             case "image":
                 canvas.addEventListener("mousemove", drawImage);
             break;
             default:
-
+                alert("Invalid tool selection");
+                // royal screw up
             break;
             }
         });
@@ -229,51 +280,64 @@ var Paint = (function(document) {
             switch(tool) {
             case "pencil":
                 if(fill) {
-                    cxt.beginPath();
-                    cxt.moveTo(points[0].x, points[0].y);
+                    ctx.beginPath();
+                    ctx.moveTo(points[0].x, points[0].y);
                     for(var i=0; i<points.length; i++) {
-                        cxt.lineTo(points[i].x, points[i].y);
+                        ctx.lineTo(points[i].x, points[i].y);
                     }
-                    cxt.fill();
-                    cxt.closePath();
+                    ctx.fill();
+                    ctx.closePath();
                     points = [];
                 }else {
                     currentLayer.getCanvasContext().drawImage(workspace, 0, 0);
                 };
+                canvas.removeEventListener("mousemove", drawPencil);
             break;
             case "brush":
-
+                canvas.removeEventListener("mousemove", drawBrush);
+                ctx.restore();
+                currentLayer.getCanvasContext().drawImage(workspace, 0, 0);
             break;
             case "circle":
+                canvas.removeEventListener("mousemove", drawCircle);
                 currentLayer.getCanvasContext().drawImage(workspace, 0, 0);
             break;
             case "square":
+                canvas.removeEventListener("mousemove", drawSquare);
                 currentLayer.getCanvasContext().drawImage(workspace, 0, 0);
             break;
             case "line":
+                canvas.removeEventListener("mousemove", drawLine);
                 currentLayer.getCanvasContext().drawImage(workspace, 0, 0);
             break;
             case "text":
-
+                canvas.removeEventListener("mousemove", drawText)
             break;
             case "eraser":
-
+                canvas.removeEventListener("mousemove", erase);
             break;
             case "dropper":
-
+                // canvas.removeEventListener("click", findColor);
+                var pix = currentLayer.getCanvasContext()
+                            .getImageData(mouse.x, mouse.y, 1, 1).data;
+                var red = parseInt(""+pix[0], 16);
+                var green = parseInt(""+pix[1], 16);
+                var blue = parseInt(""+pix[2], 16);
+                colors[0] = "#" + red + green + blue;
             break;
             case "color":
-
+                colorChooser.click();
             break;
             case "image":
-
+                canvas.removeEventListener("mousemove", drawImage);
+                currentLaye.getCanvasContext().drawImage(workspace, 0, 0);
             break;
             default:
-
+                alert("Invalid tool selection");
+                // royal screw up
             break;
             }
             ctx.clearRect(0, 0, workspace.width, workspace.height);
-            workspace.removeEventListener("mousemove", drawPencil)
         });
     }
 
